82a0d4a3ad23af2bb5714cd09dc148003304408f
Léandro DA SILVA <leothefrench@gmail.com>
2024-03-29 11:01:07 +0100

    navbar done

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+const Navbar = () => {
+  return (
+    <div className="py-5">
+      <div className="max-w-[1500px] w-[90%] mx-auto flex justify-center">
+        <Image 
+        src={'/logo.svg'}
+        width={300}
+        height={150}
+        alt="pokedex logo"
+        />
+      </div>
+    </div>
+  )
+}
+export default Navbar
